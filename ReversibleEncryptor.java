3ac63e33c8611f5e2443dae2221b16236fabb39a
RecoilGaming <qinzhaolee@gmail.com>
2025-11-03 17:45:56 -0800

    Do something

--- ReversibleEncryptor.java
+++ ReversibleEncryptor.java
@@ -5,42 +5,46 @@ import java.util.Scanner;
 
 public class ReversibleEncryptor {
 
-	/**
-	 * Ecryptor method that encrypts a given input file and writes it to a given output file.
-	 * This method is reversible, so encrypting an encrypted file will decrypt it.
-	 * @param args java commandline arguments (not used)
-	 */
-	public static void main(String... args) {
-		final String inputFile;
-		final String outputFile;
-		try (var stdin = new Scanner(System.in)) {
-			System.out.print("Enter the input file: ");
-			inputFile = stdin.nextLine();
-			System.out.print("Enter the output file: ");
-			outputFile = stdin.nextLine();
-		}
-		final String password;
-		try (var env = new FileInputStream(".env")) {
-			password = new String(env.readAllBytes());
-			assert !password.isEmpty();
-		} catch (IOException | AssertionError e) {
-			System.err.println("Password is empty or missing");
-			return;
-		}
-		try (
-			var src = new FileInputStream(inputFile);
-		) {
-			try (var out = new FileOutputStream(outputFile)) {
-				var srcBytes = src.readAllBytes();
-				var passwordBytes = password.getBytes();
-				for (int i = 0; i < srcBytes.length; i++) {
-					srcBytes[i] = (byte) (srcBytes[i] ^ (passwordBytes[i % passwordBytes.length]));
-				}
-				out.write(srcBytes);
-			}
-		} catch (IOException ioe) {
-			System.err.println(ioe.getMessage());
-			return;
-		}
-	}
+    /**
+     * Encryptor method that encrypts a given input file and writes it to a given output file.
+     * This method is reversible, so encrypting an encrypted file will decrypt it.
+     * @param args java commandline arguments (not used)
+     */
+    public static void main(String[] args) {
+        String inputFile;
+        String outputFile;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter the name of the input file: ");
+            inputFile = scanner.nextLine();
+
+            System.out.print("Enter the name of the output file: ");
+            outputFile = scanner.nextLine();
+        }
+
+        String password;
+
+        try (FileInputStream env = new FileInputStream(".env")) {
+            password = new String(env.readAllBytes());
+            assert !password.isEmpty();
+        } catch (IOException | AssertionError e) {
+            System.err.println("Password is empty or missing!");
+            return;
+        }
+
+        try (FileInputStream inp = new FileInputStream(inputFile)) {
+            try (FileOutputStream out = new FileOutputStream(outputFile)) {
+                byte[] srcBytes = inp.readAllBytes();
+                byte[] passwordBytes = password.getBytes();
+                for (int i = 0; i < srcBytes.length; i++) {
+                    srcBytes[i] = (byte) (srcBytes[i] ^
+                        (passwordBytes[i % passwordBytes.length]));
+                }
+                out.write(srcBytes);
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            return;
+        }
+    }
 }
